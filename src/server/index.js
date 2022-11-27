4db77c357414e3d0c109dd7ff75127135a8eceab
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-11-27 13:04:29 -0500

    initial setup

---
+++ src/server/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+
+const app = express();
+
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve('src/client/views/index.html'));
+});
+
+app.get('/test', (req, res) => {
+  res.send(mockAPIResponse);
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000...');
+});
