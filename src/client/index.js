5fb4acf45a5f52434ea5defe6d35b28e3dd3c572
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-11-28 13:36:52 -0500

    style the homepage

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,7 @@ import './styles/base.scss';
 import './styles/footer.scss';
 import './styles/form.scss';
 import './styles/header.scss';
+import './styles/results.scss';
 
 import { checkForName } from './js/nameChecker';
 import { handleSubmit } from './js/formHandler';
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,7 +2,7 @@ async function handleSubmit(event) {
   event.preventDefault();
 
   // check what text was put into the form field
-  let formText = document.getElementById('name').value;
+  let formText = document.querySelector('.url-input').value;
   Client.checkForName(formText);
 
   console.log('::: Form Submitted :::');
