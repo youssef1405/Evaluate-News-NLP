4fd55b82d02d192ce36141e016c93cd137d15d8b
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-11-28 18:40:11 -0500

    describe the polarity score

--- src/client/js/results.js
+++ src/client/js/results.js
@@ -4,7 +4,7 @@ const results = document.getElementById('results');
 const updateUI = (data) => {
   if (data) {
     results.innerHTML = `
-    <div id="polarity">Polarity: ${data.score_tag}</div>
+    <div id="polarity">Polarity: ${describePolarityScore(data.score_tag)}</div>
     <div id="agreement">Agreement: ${data.agreement}</div>
     <div id="subjectivity">Subjectivity: ${data.subjectivity}</div>
     <div id="confidence">Confidence: ${data.confidence}</div>
@@ -15,6 +15,32 @@ const updateUI = (data) => {
         <div id="error">⛔ It seems like the URL you entered is invalid. Please check the URL and try again ⛔</div>
     `;
   }
+};
+
+const describePolarityScore = (score) => {
+  let polarity = '';
+  switch (score) {
+    case 'P+':
+      polarity = 'Strong Positive';
+      break;
+    case 'P':
+      polarity = 'Positive';
+      break;
+    case 'NEU':
+      polarity = 'Neutral';
+      break;
+    case 'N':
+      polarity = 'Negative';
+      break;
+    case 'N+':
+      polarity = 'Strong Negative';
+      break;
+    case 'NONE':
+      polarity = 'None';
+      break;
+  }
+
+  return polarity;
 };
 
 export { updateUI };
